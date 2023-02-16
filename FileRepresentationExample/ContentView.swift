c558c1e386f6cc87519b118ab74b60bdcae69345
Iomegan <dwitt.kiel@icloud.com>
2023-02-16 10:51:08 +0100

    initial commit

--- FileRepresentationExample/ContentView.swift
+++ FileRepresentationExample/ContentView.swift
@@ -6,16 +6,39 @@
 //
 
 import SwiftUI
+import UniformTypeIdentifiers
+
+extension UTType {
+    static var exampleType = UTType(exportedAs: "com.example.FileRepresentationExample.example")
+}
+
+struct ExampleObject: Transferable {
+    let exampleTextURL = Bundle.main.url(forResource: "example", withExtension: "txt")!
+
+    static var transferRepresentation: some TransferRepresentation {
+        FileRepresentation(exportedContentType: .exampleType) { item in
+            SentTransferredFile(item.exampleTextURL)
+        }
+        // More transferring types here...
+    }
+}
 
 struct ContentView: View {
+    private let exampleObject = ExampleObject()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack {
+                Image(systemName: "doc.text")
+                Text("Try to drag me to\n Finder on macOS\nor the Files app on iOS")
+                    .multilineTextAlignment(.center)
+            }
+            .font(.title)
+            .foregroundColor(.accentColor)
+            .padding()
+            .draggable(exampleObject)
+//            .draggable(exampleObject.exampleTextURL)
+            Text("The drag will be rejected. Bug?")
         }
-        .padding()
     }
 }
 
