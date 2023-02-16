14e04aade22b9bc0a4e402688e81020c3c0609f7
Iomegan <dwitt.kiel@icloud.com>
2023-02-16 10:34:21 +0100

    Initial Commit

---
+++ FileRepresentationExample/FileRepresentationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileRepresentationExampleApp.swift
+//  FileRepresentationExample
+//
+//  Created by Daniel Witt on 16.02.23.
+//
+
+import SwiftUI
+
+@main
+struct FileRepresentationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
